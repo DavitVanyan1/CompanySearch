98895c1936dfa3f6e277f147c73e796da811743f
Davit Vanyan <david.vanyan1@gmail.com>
2026-05-24 14:53:29 +0400

    initial commit, skeleton of the project created

---
+++ src/test/java/com/davitvanyan/companysearch/CompanysearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davitvanyan.companysearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanysearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
